53931189df9997b4f0138be36f9c7f9a650d8eb5
higormonteiro <higor.tecinfor@gmail.com>
2020-02-08 12:43:54 -0300

    heroku deploy

--- backend_api/settings.py
+++ backend_api/settings.py
@@ -123,4 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
+
+DATABASES = { 'default': db_url.config(ssl_require=False) }
