7c5876b7b84736235bfe73c7d0e739325c1194b8
higormonteiro <higor.tecinfor@gmail.com>
2020-02-11 18:44:36 -0300

    created new endpoint to clients

--- client/urls.py
+++ client/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import CreateView, DetailsView
+from . import viewset
 
 urlpatterns = {
     url(r'^clients/$', CreateView.as_view(), name="create"),
     url(r'^client/(?P<pk>[0-9]+)/$',
         DetailsView.as_view(), name="details"),
+    url(r'^clients/$', viewset.client_list, name="create"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
---
+++ client/viewset.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+
+from .serializers import ClientSerializer
+from .models import Client
+
+
+@csrf_exempt
+def client_list(request):
+    if request.method == 'GET':
+        clients = Client.objects.all()
+        clients_serializer = ClientSerializer(clients, many=True)
+        return JsonResponse(clients_serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        client_data = JSONParser().parse(request)
+        client_serializer = ClientSerializer(data=client_data)
+        if client_serializer.is_valid():
+            client_serializer.save()
+            return JsonResponse(
+                client_serializer.data,
+                status=status.HTTP_201_CREATED
+                )
+        return JsonResponse(
+            client_serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+
+    elif request.method == 'DELETE':
+        Client.objects.all().delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
